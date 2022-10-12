d00351d8fcddc6302e8bbfe66e2d594535345685
Michael Lee <mygosity@gmail.com>
2022-10-13 10:11:16 +1100

    updated create script for windows

--- create.sh
+++ create.sh
@@ -1,7 +1,30 @@
-PROJECT=$1
-CMAKETOOLCHAIN=
+function createForPlatform {
+    BUILD_TYPE=$1
 
-rm -rf project
-mkdir project
-cd project
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug "$CMAKETOOLCHAIN" ../src
+    rm -rf project
+    mkdir project
+    cd project
+
+    if [ "${BUILD_TYPE}" == "r" ]; then
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+    else
+        cmake -DCMAKE_BUILD_TYPE=Debug ..
+    fi
+}
+
+case $1 in
+    w)  echo "*************** installing windows scripts *************** "
+        createForPlatform $2
+        start ./cpp-playground.vcxproj
+        ;;
+    m)  echo "*************** installing mac scripts *************** "
+        createForPlatform $2
+        ;;
+    l)  echo "*************** installing linux scripts - assuming ubuntu18.04 *************** "
+        createForPlatform $2
+        ;;
+    *)
+        echo "*************** Required argument missing"
+        echo "*************** use an argument of either w/l/m for windows/linux/mac | Example usage: sh install.sh m or ./install.sh l *************** "
+        ;;
+esac
